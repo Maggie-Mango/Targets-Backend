524911b984131eafb2e14da4f5558ff8e98db98e
Maggie Saldivia <Maggiesaldivia@gmail.com>
2022-11-01 15:23:28 -0400

    Delete config.js

--- database/config.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-
-const db = new Sequelize('case_targets', 'root', 'Square2020!', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-export default db;
