7e726fc9106a1622b92c6f473210dba8b54366f3
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-11-10 19:52:23 -0500

    refine queries

--- controllers/LeadsView/snowflake.js
+++ controllers/LeadsView/snowflake.js
@@ -4,17 +4,37 @@ import query from '../../database/fourWeekAvg.js'
 
 var connection = snowflake.createConnection(config)
 
-
 export const getAvg = async(req, res) =>{
     connection.execute({
     sqlText: query,
     complete: async function(err,stmt,rows){
-        let pool = await connection.connectAsync();
-        console.log(rows)     
-        res.send(rows);
+        console.log('fetching snowflake data...')
+        let pool = await connection.connectAsync(
+            function(err, conn) {
+                if (err) {
+                    console.error('Unable to connect: ' + err.message);
+                } 
+                else {
+                    let connId = conn.getId();
+                    console.log('Successfully connected to Snowflake on id ' + connId); 
+                }
+            }).then( () => {
+                connection.execute({
+                sqlText: query,
+                complete: function(err, stmt, rows) {
+                    if (err) {
+                        console.error('error: ' + err.message);
+                    } else {
+                        console.log(rows)
+                        res.send(rows);
+                    }
+                }
+            })
+        })     
     }
 })
 }
+
 /*
 export const data = connection.connectAsync( 
     function(err, conn) {
--- models/snowflake.js
+++
@@ -1,4 +0,0 @@
-import { QueryTypes } from require('sequelize');
-import query from '../controllers/LeadsView/snowflake.js'
-
-export const data = await sequelize.query(query, { type: QueryTypes.SELECT });
